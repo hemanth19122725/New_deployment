d4efe11277cee7fed19e5a56b63b993113d1a4dd
Salman-hub-15 <salmonbenni@gmail.com>
2025-08-05 23:22:12 +0530

    Frontend Modified

--- frontend/new-dp/src/app/dp-comp/dp-comp.component.ts
+++ frontend/new-dp/src/app/dp-comp/dp-comp.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
@@ -9,10 +9,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './dp-comp.component.css'
 })
 
-export class DpCompComponent {
+export class DpCompComponent{
   deployForm: FormGroup;
 
   dp_type: boolean = false;
+
+  ipLabel: string = 'IP Address';
+  deploymentType: string = '';
+
+  readonly CLOUD_TYPES = ['aws', 'gcp', 'azure'];
+  
+  // labelName: string = 'IP Address';
 
   constructor(private fb: FormBuilder) {
     this.deployForm = this.fb.group({
@@ -29,50 +36,60 @@ export class DpCompComponent {
     });
 
 
-    this.deployForm.get('deployType')?.valueChanges.subscribe((value:string)=>{
-      this.dp_type = ['aws','azure','gcp'].includes(value.toLowerCase());
+  //   this.deployForm.get('deployType')?.valueChanges.subscribe((value:string)=>{
+  //     this.dp_type = ['aws','azure','gcp'].includes(value.toLowerCase());
 
-      const hostOrIpControl = this.deployForm.get('ipAddress');
-      if (this.dp_type) {
-        hostOrIpControl?.setValidators([Validators.required]); // Only require
-      } else {
-        hostOrIpControl?.setValidators([
+  //     const hostOrIpControl = this.deployForm.get('ipAddress');
+  //     if (this.dp_type) {
+  //       hostOrIpControl?.setValidators([Validators.required]); // Only require
+  //     } else {
+  //       hostOrIpControl?.setValidators([
+  //         Validators.required,
+  //         Validators.pattern('^([0-9]{1,3}\\.){3}[0-9]{1,3}$')
+  //       ]);
+  //     }
+  //     hostOrIpControl?.updateValueAndValidity();
+    
+  // })
+  }
+
+
+  // selectedDeployType: string = 'aws'
+  // ipLabel: string = 'IP Address';
+
+//   updateLabel(): void {
+//   const selectedValue = this.deployForm.get('scriptLocation')?.value?.toLowerCase();
+//   this.deploymentType = selectedValue;
+
+//   const isCloud = ['aws', 'gcp', 'azure'].includes(selectedValue);
+//   this.ipLabel = isCloud ? 'Host' : 'IP Address';
+// }
+
+
+  ngOnInit(): void {
+    this.deployForm.get('scriptLocation')?.valueChanges.subscribe((value: string) => {
+      const selected = value?.toLowerCase();
+      const ipCtrl = this.deployForm.get('ipAddress');
+
+      // Change label
+      if (this.CLOUD_TYPES.includes(selected)) {
+        this.ipLabel = 'Host';
+        ipCtrl?.setValidators([Validators.required]); 
+      } 
+      
+      else {
+        this.ipLabel = 'IP Address';
+        ipCtrl?.setValidators([
           Validators.required,
-          Validators.pattern('^([0-9]{1,3}\\.){3}[0-9]{1,3}$')
+          Validators.pattern('^([0-9]{1,3}\\.){3}[0-9]{1,3}$') 
         ]);
       }
-      hostOrIpControl?.updateValueAndValidity();
-    
-  })
 
+      ipCtrl?.updateValueAndValidity();
+    });
   }
 
-   deploymentType: string = '';
-  ipAddress: string = '';
-  showIPAddressField: boolean = false;
 
-  onDeploymentTypeChange(event: any) {
-    this.showIPAddressField = (this.deploymentType === 'vm' || this.deploymentType === 'custom');
-  }
-
-  isCloudType(type: string): boolean {
-    return type === 'aws' || type === 'gcp' || type === 'azure';
-  }
-
-  selectedDeployType: string = 'aws'
-  ipLabel: string = 'IP Address';
-  updateLabel(): void {
-  const selectedValue = this.deployForm.get('scriptLocation')?.value?.toLowerCase();
-  this.deploymentType = selectedValue;
-
-  const isCloud = ['aws', 'gcp', 'azure'].includes(selectedValue);
-  this.ipLabel = isCloud ? 'Host' : 'IP Address';
-}
-
-
-
-
-  
 
   onSubmit() {
     if (this.deployForm.valid) {
